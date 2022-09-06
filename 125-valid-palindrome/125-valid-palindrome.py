3182284989236ad49c6125c29419ea9e5eeba32f
Morgan Lyle <88851037+morganlyle@users.noreply.github.com>
2022-09-06 13:29:34 -0400

    Time: 1691 ms (15.36%), Space: 15 MB (37.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -4,21 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l, r = 0, len(s) - 1
-        while l < r:
-            while l < r and not self.alphanum(s[l]):
-                l += 1
-            while l < r and not self.alphanum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l += 1
-            r -= 1
-        return True
+        x = ''
         
-    def alphanum(self, c):
-        return (
-            ord("A") <= ord(c) <= ord("Z")
-            or ord("a") <= ord(c) <= ord("z")
-            or ord("0") <= ord(c) <= ord("9")
-        )
+        for i in s:
+            if i.isalnum():
+                x += i.lower()
+        return x == x[::-1]
