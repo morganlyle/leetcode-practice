9ddfebd55defcbe6c108a55d882b115c75fd3559
Morgan Lyle <88851037+morganlyle@users.noreply.github.com>
2022-09-22 14:43:34 -0400

    Time: 105 ms (42.36%), Space: 14.7 MB (9.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -9,7 +9,7 @@ class Solution(object):
         max_count = 0
         start = 0
         
-        for i in range(0, len(s)):
+        for i in range(len(s)):
             if s[i] in last:
                 start = max(start, last[s[i]] + 1)
             max_count = max(max_count, i - start + 1)
